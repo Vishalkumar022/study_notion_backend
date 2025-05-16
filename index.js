d904e7c6d75283334baec66d932b87087c520ce6
Vishal Kumar <thingswaste850@gmail.com>
2025-05-17 00:24:30 +0530

    Update index.js

--- index.js
+++ index.js
@@ -27,7 +27,7 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(
 	cors({
-		origin: ["https://study-notion-jet-iota.vercel.app","http://localhost:3000"],
+		origin: ["https://study-notion-jet-iota.vercel.app","http://localhost:3000","https://studynotionfrontend-eight.vercel.app"],
 		credentials: true,
 	})
 );
